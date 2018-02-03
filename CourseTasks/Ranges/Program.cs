3154db0c9733a74c01663ac3e105a4bf058d1aa4
petr <karabutp@mail.ru>
2018-02-04 00:31:39 +0700

    Проект Ranges: добавлен класс Range и простая программа с ним.

--- CourseTasks/Ranges/Program.cs
+++ CourseTasks/Ranges/Program.cs
@@ -10,6 +10,37 @@ namespace Ranges
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите начало первого интервала");
+            var from1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конец первого интервала");
+            var to1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите начало второго интервала");
+            var from2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конец второго интервала");
+            var to2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            var range1 = new Range(from1, to1);
+            var range2 = new Range(from2, to2);
+
+            var intersection = range1.GetIntersection(range2);
+            if (intersection != null)
+            {
+                Console.WriteLine("Пересечение: {0}", intersection.Note);
+            }
+            else
+            {
+                Console.WriteLine("Интервалы не пересекаются");
+            }
+
+            Console.WriteLine("Объединение: {0}", Range.GetNotes(range1.GetAssociation(range2)));
+
+            Console.WriteLine("Разность: {0}", Range.GetNotes(range1.GetDifference(range2)));
+
+            Console.ReadKey();
         }
     }
 }
---
+++ CourseTasks/Ranges/Range.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranges
+{
+    class Range
+    {
+        private double from;
+        private double to;
+        public Range(double from, double to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return to - from;
+            }
+        }
+
+        public bool IsInside(double x)
+        {
+            return (x >= from && x <= to);
+        }
+
+        public Range GetIntersection(Range other)
+        {
+            if (IsInside(other.from))
+            {
+                return new Range(other.from, Math.Min(to, other.to));
+            }
+            else if (other.IsInside(from))
+            {
+                return new Range(Math.Max(from, other.from), other.to);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Range[] GetAssociation(Range other)
+        {
+            if (GetIntersection(other) != null)
+            {
+                return new Range[] { new Range(Math.Min(from, other.from), Math.Max(to, other.to)) };
+            }
+            else
+            {
+                return new Range[] { this, other };
+            }
+        }
+
+        public Range[] GetDifference(Range other)
+        {
+            var fromIsInside = IsInside(other.from);
+            var toIsInside = IsInside(other.to);
+            var range1 = new Range(from, other.from);
+            var range2 = new Range(other.to, to);
+
+            if (fromIsInside && toIsInside)
+            {
+                return range1.GetAssociation(range2);
+            }
+            else if (fromIsInside)
+            {
+                return new Range[] { range1 };
+            }
+            else if (toIsInside)
+            {
+                return new Range[] { range2 };
+            }
+            else
+            {
+                return new Range[] { this };
+            }
+        }
+
+        public string Note
+        {
+            get
+            {
+                return string.Format("[{0};{1}]", from, to);
+            }
+        }
+
+        public static string GetNotes(Range[] ranges)
+        {
+            var notes = new string[ranges.Length];
+            for (int i = 0; i < ranges.Length; i++)
+            {
+                notes[i] = ranges[i].Note;
+            }
+
+            return string.Join(" U ", notes);
+        }
+    }
+}
