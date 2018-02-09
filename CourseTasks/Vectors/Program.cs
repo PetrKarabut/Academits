9dd06106888afec9e62860be807a18a292b22e25
petr <karabutp@mail.ru>
2018-02-10 03:05:18 +0700

    Добавлен проект Vectors

---
+++ CourseTasks/Vectors/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vector1 = new Vector(3);
+            var array = new[] { 5d, -15d, 6d, 8.56d, 57d };
+            var vector2 = new Vector(array);
+            var vector3 = new Vector(3, array);
+            var vector4 = new Vector(vector3);
+
+            WriteInfo(vector1);
+            WriteInfo(vector2);
+            WriteInfo(vector3);
+            WriteInfo(vector4);
+
+            Console.ReadKey();
+            Console.Clear();
+
+            var vector5 = new Vector(new[] { 1d, -8d, 7d });
+            var vector6 = new Vector(new[] { 2d, 4d, -12d, 6 });
+
+            WriteInfo(vector5);
+            WriteInfo(vector6);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Сумма: {0}", Vector.Sum(vector5, vector6));
+            Console.WriteLine("Разность: {0}", Vector.Difference(vector5, vector6));
+            Console.WriteLine("Скалярное произведение: {0}", Vector.Dot(vector5, vector6));
+
+            Console.ReadKey();
+        }
+
+        private static void WriteInfo(Vector vector)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Вектор: {0}", vector);
+            Console.WriteLine("Размерность: {0}", vector.Size);
+            Console.WriteLine("Длина: {0}", vector.Magnitude);
+            Console.WriteLine("Обратный вектор: {0}", vector.Opposite);
+        }
+    }
+}
---
+++ CourseTasks/Vectors/Vector.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectors
+{
+    class Vector
+    {
+        private double[] components;
+
+        public Vector(int size)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            components = new double[size];
+        }
+
+        public Vector(int size, double[] array) : this(size)
+        {
+            var minimum = Math.Min(size, array.Length);
+            for (var i = 0; i < minimum; i++)
+            {
+                components[i] = array[i];
+            }
+        }
+
+        public Vector(double[] array) : this(array.Length, array)
+        {
+        }
+
+        public Vector(Vector other) : this(other.components)
+        {
+        }
+
+        public int Size => components.Length;
+
+        public override string ToString()
+        {
+            return $"({string.Join(";", components)})";
+        }
+
+        public Vector Cut(int size)
+        {
+            return new Vector(size, components);
+        }
+
+        public Vector Plus(Vector other)
+        {
+            var maximumSize = Math.Max(Size, other.Size);
+
+            var vector1 = Cut(maximumSize);
+            var vector2 = other.Cut(maximumSize);
+
+            for (var i = 0; i < maximumSize; i++)
+            {
+                vector1.components[i] += vector2.components[i];
+            }
+
+            return vector1;
+        }
+
+        public Vector Minus(Vector other)
+        {
+            var maximumSize = Math.Max(Size, other.Size);
+
+            var vector1 = Cut(maximumSize);
+            var vector2 = other.Cut(maximumSize);
+
+            for (var i = 0; i < maximumSize; i++)
+            {
+                vector1.components[i] -= vector2.components[i];
+            }
+
+            return vector1;
+        }
+
+        public Vector Multiply(double x)
+        {
+            var vector = new Vector(this);
+
+            for (var i = 0; i < Size; i++)
+            {
+                vector.components[i] *= x;
+            }
+
+            return vector;
+        }
+
+        public Vector Opposite => Multiply(-1);
+
+        public double Magnitude => Math.Sqrt(Dot(this, this));
+
+        public double GetComponent(int i)
+        {
+            return components[i];
+        }
+
+        public void SetComponent(int i, double value)
+        {
+            components[i] = value;
+        }
+
+        public const double epsilon = double.Epsilon * 100;
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            var other = obj as Vector;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Size == other.Size && Minus(other).Magnitude < epsilon;
+        }
+
+        public override int GetHashCode()
+        {
+            return Size;
+        }
+
+        public static double Dot(Vector v, Vector w)
+        {
+            var maximumSize = Math.Max(v.Size, w.Size);
+
+            var vector1 = v.Cut(maximumSize);
+            var vector2 = w.Cut(maximumSize);
+
+            double dot = 0;
+            for (var i = 0; i < maximumSize; i++)
+            {
+                dot += vector1.components[i] * vector2.components[i];
+            }
+
+            return dot;
+        }
+
+        public static Vector Sum(Vector vector1, Vector vector2)
+        {
+            return vector1.Plus(vector2);
+        }
+
+        public static Vector Difference(Vector vector1, Vector vector2)
+        {
+            return vector1.Minus(vector2);
+        }
+    }
+}
