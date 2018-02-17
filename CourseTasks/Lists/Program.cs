c2980a7bb728108cc69078103d9cc16a5a02b5e1
petr <karabutp@mail.ru>
2018-02-17 17:15:15 +0700

    Проект Lists: доделан класс SimplyConnectedList

--- CourseTasks/Lists/Program.cs
+++ CourseTasks/Lists/Program.cs
@@ -10,6 +10,55 @@ namespace Lists
     {
         static void Main(string[] args)
         {
+            var size = 7;
+            var range = 99;
+
+            var array = new string[size];
+
+            var random = new Random();
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-range, range).ToString();
+            }
+
+            var list = new SimplyConnectedList<string>();
+            list.InsertInBegining(array[0]);
+            for (var i = 1; i < array.Length; i++)
+            {
+                list.Insert(array[i], i);
+            }
+
+            Console.WriteLine("задан односвязный список: {0}", list);
+            Console.WriteLine();
+
+            Console.WriteLine("длина списка {0}; первый элемент {1}; второй элемент {2}", list.Count, list.First, list.GetValue(1));
+            Console.WriteLine();
+            Console.WriteLine("после замены четвертого элемента равного {0} на {1} получим", list.SetValue(3, "N"), "N");
+            Console.WriteLine(list);
+            Console.WriteLine();
+            Console.WriteLine("после удаления пятого элемента равного {0} получим", list.RemoveAt(4));
+            Console.WriteLine(list);
+            Console.WriteLine();
+            Console.WriteLine("после добавления в начало элемента равного {0} получим", "B");
+            list.InsertInBegining("B");
+            Console.WriteLine(list);
+            Console.WriteLine();
+            Console.WriteLine("после добавления по номеру 3 элемента равного {0} получим", "I");
+            list.Insert("I", 2);
+            Console.WriteLine(list);
+            Console.WriteLine();
+            Console.WriteLine("после удаления элемента равного {0} получим", "I");
+            list.Delete("I");
+            Console.WriteLine(list);
+            Console.WriteLine();
+            Console.WriteLine("после удаления первого элемента равного {0} получим", list.DeleteFirst());
+            Console.WriteLine(list);
+            Console.WriteLine();
+            Console.WriteLine("после разворота списка получим");
+            list.Reverse();
+            Console.WriteLine(list);
+
+            Console.ReadKey();
         }
     }
 }
--- CourseTasks/Lists/SimplyConnectedList.cs
+++ CourseTasks/Lists/SimplyConnectedList.cs
@@ -121,6 +121,118 @@ namespace Lists
             Count--;
             return deletedValue;
         }
-       
+
+        public void InsertInBegining(T value)
+        {
+            var unit = new Unit(value);
+            if (Count > 1)
+            {
+                unit.Next = head;
+            }
+
+            head = unit;
+            Count++;
+        }
+
+        public void Insert(T value, int index)
+        {
+            if (index == 0)
+            {
+                InsertInBegining(value);
+                return;
+            }
+
+            if (head == null || index < 0 || index > Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var previous = head;
+            for (var i = 0; i < Count; i++)
+            {
+                if (i == index - 1)
+                {
+                    break;
+                }
+                previous = previous.Next;
+            }
+
+            var unit = new Unit(value);
+            unit.Next = previous.Next;
+            previous.Next = unit;
+            Count++;
+        }
+
+        public bool Delete(T value)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+
+            if (head.Value.Equals(value))
+            {
+                head = head.Next;
+                Count--;
+                return true;
+            }
+
+            var previous = head;
+            for (var i = 0; i < Count - 1; i++)
+            {
+                if (previous.Next.Value.Equals(value))
+                {
+                    previous.Next = previous.Next.Next;
+                    Count--;
+                    return true;
+                }
+                previous = previous.Next;
+            }
+
+            return false;
+        }
+
+        public T DeleteFirst()
+        {
+            if (head == null)
+            {
+                throw new NullReferenceException();
+            }
+            var value = head.Value;
+            head = head.Next;
+            Count--;
+            return value;
+        }
+
+        public void Reverse()
+        {
+            if (Count < 2)
+            {
+                return;
+            }
+
+            var end = head;
+            while (end.Next != null)
+            {
+                var next1 = end.Next;
+                end.Next = next1.Next;
+                next1.Next = head;
+                head = next1;
+            }
+        }
+
+        public override string ToString()
+        {
+            var unit = head;
+            var builder = new StringBuilder();
+            while (unit != null)
+            {
+                builder.Append(unit.Value).Append(" ");
+                unit = unit.Next;
+            }
+
+            return builder.ToString();
+        }
+
     }
 }
