5a3cd0f83a3e62234908b941176d5e98f75f3fec
petr <karabutp@mail.ru>
2018-02-21 02:44:05 +0700

    Добавлен проект ArrayLists

---
+++ CourseTasks/ArrayLists/ListOnArray.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLists
+{
+    class ListOnArray<T> : IList<T>
+    {
+        private T[] array;
+
+        private int capacity;
+
+        public ListOnArray(int capacity)
+        {
+            this.capacity = capacity;
+            array = new T[capacity];
+        }
+        
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new IndexOutOfRangeException("Ошибка: индекс вышел за пределы списка");
+                }
+
+                return array[index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new IndexOutOfRangeException("Ошибка: индекс вышел за пределы списка");
+                }
+
+                array[index] = value;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new ListEnumerator<T>(array, Count);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new ListEnumerator<T>(array, Count);
+        }
+
+        public int IndexOf(T item)
+        {
+            for (var i = 0; i < Count; i++)
+            {
+                if (Equals(array[i], item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException("Ошибка: индекс вышел за пределы списка");
+            }
+
+            if (Count == array.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            for (var i = Count; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[index] = item;
+            Count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException("Ошибка: индекс вышел за пределы списка");
+            }
+
+            for (var i = index; i < Count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            Count--;
+        }
+
+        public void Add(T item)
+        {
+            if (Count == array.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            array[Count] = item;
+            Count++;
+        }
+
+        private void IncreaseCapacity()
+        {
+            Capacity = 2 * Count;
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+
+            set
+            {
+                if (value < Count)
+                {
+                    throw new ArgumentException("нельзя задать вместимость меньше длины списка");
+                }
+
+                var old = array;
+                array = new T[value];
+                Array.Copy(old, array, Count);
+                capacity = value;
+            }
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return IndexOf(item) != -1;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (arrayIndex < 0 || arrayIndex > array.Length)
+            {
+                throw new IndexOutOfRangeException("Ошибка: индекс вышел за пределы массива");
+            }
+
+            var i = arrayIndex;
+
+            foreach (var t in this.array)
+            {
+                array[i] = t;
+                i++;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            var index = IndexOf(item);
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            RemoveAt(index);
+            return true;
+        }
+
+        public void TrimExcess()
+        {
+            if (Count > 0.9f * array.Length)
+            {
+                return;
+            }
+
+            var old = array;
+            array = new T[Count];
+            Array.Copy(old, array, Count);
+            capacity = Count;
+        }
+    }
+}
---
+++ CourseTasks/ArrayLists/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArrayLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ListOnArray<string>(20);
+
+            list.Add("A");
+            list.Add("B");
+            list.Add("C");
+            list.Add("A");
+            list.Add("D");
+            list.Add("F");
+           
+            WriteList(list, "задан список");
+
+            list.TrimExcess();
+            WriteList(list, "после урезания внутреннего массива");
+
+            list.Remove("A");
+            WriteList(list, "после удаления элемента A");
+
+            list.Add("E");
+            WriteList(list, "после добавления элемента E");
+
+            list.Add("G");
+            WriteList(list, "после добавления элемента G");
+
+            list.RemoveAt(2);
+            WriteList(list, "после удаления 3-го элемента");
+
+            list.Capacity = 8;
+            WriteList(list, "после установки длины внутреннего массива равной 8");
+
+            list.Insert(1, "P");
+            WriteList(list, "после вставления P на второе место");
+
+            Console.WriteLine("индекс элемента F равен {0}", list.IndexOf("F"));
+            Console.WriteLine();
+
+            Console.WriteLine("содержится ли элемент H  {0}", list.Contains("H"));
+            Console.WriteLine();
+
+            list.Clear();
+            WriteList(list, "после очищения списка");
+
+
+
+            Console.ReadKey();
+        }
+
+        private static void WriteList(ListOnArray<string> list, string title)
+        {
+            Console.WriteLine(title);
+            Console.WriteLine("список: " + string.Join(" ", list) + $"    Count = {list.Count} ; Capacity = {list.Capacity}");
+            Console.WriteLine();
+        }
+
+      
+    }
+}
